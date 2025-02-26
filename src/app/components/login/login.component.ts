785dac5dba99de0551d76a4c6b5942176532b59a
voidcht <chathurangilansakara@gmail.com>
2025-02-27 04:51:55 +0530

    SignInTemplate

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css','../../../styles.css']
+})
+export class LoginComponent {
+
+}
